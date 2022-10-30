567bb27ed404cb0a49108c76d22cf623ae3f13f1
CrimsonTLU <chris55@tlu.ee>
2022-10-30 12:25:19 +0200

    Homepage template (closes #4)

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./App.css"
+import Home from "./pages/Home.js"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
