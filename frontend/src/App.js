f680fba46c0d963fb83d26cf973d9df319edeaeb
CrimsonTLU <chris55@tlu.ee>
2022-10-30 15:08:40 +0200

    Portfolio template (closes #6)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
 import Home from "./pages/Home.js"
 import News from "./pages/News.js"
+<<<<<<< Updated upstream
+=======
+import Portfolio from "./pages/Portfolio.js"
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
@@ -16,6 +20,13 @@ export default function App() {
           path="/news"
           element={<News />}
         />
+<<<<<<< Updated upstream
+=======
+        <Route
+          path="/projects"
+          element={<Portfolio />}
+        />
+>>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import "../App.css"
 import { Box, Typography } from "@mui/material"
+<<<<<<< Updated upstream
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -8,6 +9,13 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 
 const Home = () => {
+=======
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+
+const News = () => {
+>>>>>>> Stashed changes
   return (
     <Box
       sx={{
@@ -22,6 +30,7 @@ const Home = () => {
       <Typography>Banner</Typography>
       <Typography>Navmenu</Typography>
       <Typography>All news</Typography>
+<<<<<<< Updated upstream
       <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <ListItem>
         <ListItemText primary="News 1" secondary="News 1 contents" />
@@ -30,8 +39,28 @@ const Home = () => {
         <ListItemText primary="News 2" secondary="News 2 contents" />
       </ListItem>
     </List>
+=======
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <ListItem>
+          <ListItemText
+            primary="News 1"
+            secondary="News 1 contents"
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary="News 2"
+            secondary="News 2 contents"
+          />
+        </ListItem>
+      </List>
+>>>>>>> Stashed changes
     </Box>
   )
 }
 
+<<<<<<< Updated upstream
 export default Home
+=======
+export default News
+>>>>>>> Stashed changes
