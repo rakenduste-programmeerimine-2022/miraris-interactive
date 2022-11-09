9f33e40199d72a6fe32858649b2f61e68f51cebb
robkukke <rkukke@tlu.ee>
2022-11-09 19:24:36 +0200

    Signup BE and FE (closes #11)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/error.middleware")
 const connectDB = require("./utility/db")
 const port = process.env.PORT || 8080
 
@@ -9,5 +10,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use("/api/users", require("./routes/user.routes"))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
 import "../App.css"
-import { Box, TextField, Typography, Button } from "@mui/material"
+import {
+  Box,
+  TextField,
+  Typography,
+  Button,
+  FormControl,
+  FormGroup,
+  InputLabel,
+  Input
+} from "@mui/material"
 import { ThemeProvider } from "@mui/material/styles"
 import Header from "../components/Header"
 import Theme from "../components/Theme"
 
-const Login = () => {
+const Signup = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const { signup, error, isLoading } = useSignup()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    await signup(name, email, password)
+  }
+
   return (
     <ThemeProvider theme={Theme}>
       <Box
@@ -19,22 +40,47 @@ const Login = () => {
         }}
       >
         <Header></Header>
-        <Typography>Signup</Typography>
-        <Typography>Username</Typography>
-        <TextField></TextField>
-        <Typography>email</Typography>
-        <TextField></TextField>
-        <Typography>Password</Typography>
-        <TextField></TextField>
-        <Button
-          color="success"
-          variant="contained"
-        >
-          Signup
-        </Button>
+        <Typography>Sign up</Typography>
+        <form onSubmit={handleSubmit}>
+          <FormGroup>
+            <FormControl required>
+              <InputLabel>Name</InputLabel>
+              <Input
+                type="text"
+                onChange={e => setName(e.target.value)}
+                value={name}
+              />
+            </FormControl>
+            <FormControl required>
+              <InputLabel>Email address</InputLabel>
+              <Input
+                type="email"
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+              />
+            </FormControl>
+            <FormControl required>
+              <InputLabel>Password</InputLabel>
+              <Input
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+              />
+            </FormControl>
+            <Button
+              color="success"
+              variant="contained"
+              type="submit"
+              disabled={isLoading}
+            >
+              Sign up
+            </Button>
+          </FormGroup>
+          {error && <Box>{error}</Box>}
+        </form>
       </Box>
     </ThemeProvider>
   )
 }
 
-export default Login
+export default Signup
