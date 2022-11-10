495c0a2b6d2464647cc729d7316711f4caeea5e6
robkukke <rkukke@tlu.ee>
2022-11-10 17:36:10 +0200

    Login, Logout BE and FE (closes #12)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,12 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { AuthContextProvider } from "./context/AuthContext"
-import { ProjectsContextProvider } from "./context/ProjectContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <ProjectsContextProvider>
-        <App />
-      </ProjectsContextProvider>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>
 )
