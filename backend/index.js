e0bba8c9eb45e3e82429cdd12f1e71b8b42d49db
CrimsonTLU <chris55@tlu.ee>
2022-11-14 17:58:46 +0200

    server connection fixed

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,11 @@ connectDB()
 
 const app = express()
 
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    next()
+})
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const { registerUser, loginUser } = require("../controllers/user.controller")
+const proxyMiddleware = require("../middleware/proxy.middleware")
 
-router.post("/", registerUser)
-router.post("/login", loginUser)
+router.post("/", proxyMiddleware, registerUser)
+router.post("/login", proxyMiddleware, loginUser)
 
 module.exports = router
