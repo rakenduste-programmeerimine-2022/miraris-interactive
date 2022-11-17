371b75fa6dcac1e8547a864b33fd2565cf550458
Christian-Enrique Hindremäe <chris55@tlu.ee>
2022-11-17 11:53:56 +0200

    Portfolio WIP (closes #18, closes #20, closes #21, closes #30)

--- frontend/src/components/ProjectDetails.js
+++ frontend/src/components/ProjectDetails.js
@@ -1,10 +1,13 @@
 import ListItem from "@mui/material/ListItem"
 import ListItemText from "@mui/material/ListItemText"
+import { Button } from "@mui/material"
 
 import { useProjectsContext } from "../hooks/useProjectsContext"
 
 const ProjectDetails = ({ project }) => {
   const { dispatch } = useProjectsContext()
+  const title = project.title
+  console.log(title)
 
   return (
     <ListItem>
@@ -12,6 +15,14 @@ const ProjectDetails = ({ project }) => {
         primary={project.title}
         secondary={project.description}
       ></ListItemText>
+      <Button
+        onClick={e => {
+          e.preventDefault()
+          window.location.href = "projects/" + title
+        }}
+      >
+        Read more
+      </Button>
     </ListItem>
   )
 }
