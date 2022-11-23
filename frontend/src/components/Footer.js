f5977d9c2fc6ef0d7fb19403c2a91abbb22c045e
CrimsonTLU <chris55@tlu.ee>
2022-11-23 18:27:11 +0200

    Design Improvements, Footer (closes #58)

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import { Typography, Box } from "@mui/material"
+import "../App.css"
+
+export default function Header() {
+    return (
+        <Box className="footer">
+            <Typography>Miraris Interactive 2022</Typography>
+        </Box>
+    )
+}
