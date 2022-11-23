6a9bc6ac3d42c1d1855746ed2099b09cfd225156
CrimsonTLU <chris55@tlu.ee>
2022-11-23 18:38:36 +0200

    News FE WIP

--- frontend/src/pages/NewsPage.js
+++ frontend/src/pages/NewsPage.js
@@ -1,36 +1,30 @@
 import React from "react"
 import "../App.css"
-import { Box, Typography } from "@mui/material"
+import { Box, TextField, Typography, Button } from "@mui/material"
 import { ThemeProvider } from "@mui/material/styles"
 import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
 import ListItemText from "@mui/material/ListItemText"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 import Theme from "../components/Theme"
 
 const NewsPage = () => {
   return (
     <ThemeProvider theme={Theme}>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "top",
-          flexDirection: "column",
-          gap: "25px",
-          height: "10vh"
-        }}
-      >
-        <Typography>Banner</Typography>
+      <Box className="mainContainer">
         <Header></Header>
         <Typography>News title</Typography>
-        <List
-          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-        >
+        <List>
           <ListItem>
             <ListItemText primary="News contents" />
           </ListItem>
         </List>
+        <Box>
+          <TextField></TextField>
+          <Button>Add comment</Button>
+        </Box>
+        <Footer></Footer>
       </Box>
     </ThemeProvider>
   )
