2d80d768b2b01b2f13870599854a494d6faa8f56
robkukke <rkukke@tlu.ee>
2022-11-24 18:40:41 +0200

    News comments WIP

---
+++ backend/models/comment.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const commentSchema = mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
+    },
+    comment: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model("Comment", commentSchema)
--- backend/routes/news.routes.js
+++ backend/routes/news.routes.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router()
-const { getNews, getNewsById } = require("../controllers/news.controller")
+const {
+  getNews,
+  getNewsById,
+  setComment
+} = require("../controllers/news.controller")
+const { protect } = require("../middleware/auth.middleware")
 
 router.get("/", getNews)
 router.get("/:id", getNewsById)
+router.post("/", protect, setComment)
 
 module.exports = router
