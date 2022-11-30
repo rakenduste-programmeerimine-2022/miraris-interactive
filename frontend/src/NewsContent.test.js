f51fbe79862e8a1c71f62aa7021d83cbed21d03c
Christian-Enrique Hindremäe <chris55@tlu.ee>
2022-11-30 20:00:39 +0200

    test WIP

---
+++ frontend/src/NewsContent.test.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { render, unmountComponentAtNode } from "react-dom"
+import { act } from "react-dom/test-utils"
+
+import NewsContent from "./components/NewsContent"
+
+let container = null
+let newsElement = {
+  header: "mockNews",
+  introduction: "mockNewsIntroduction",
+  body: "mockNewsBody",
+}
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div")
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+it("renders with given test header, introduction, and body", () => {
+  act(() => {
+    render(NewsContent = ({ newsElement }), container)
+  })
+  expect(container.textContent).toBe(
+    "mockNews",
+    "mockNewsIntroduction",
+    "mockNewsBody"
+    )
+})
